288a44b5f62c69ce12a1e52c11de4e88d4489988
Leonardo Borges <leonardo.appelt.boegs@gmail.com>
2024-11-01 23:56:32 -0300

    finalização do entregavel calculadora dodev

--- src/script.js
+++ src/script.js
@@ -1,11 +1,38 @@
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-//ESCREVA SEU CÓDIGO AQUI!!
+function calculadora(n1, n2, operador) {
+  let resultado
+  let num1 = parseInt(n1)
+  let num2 = parseInt(n2)
 
+  switch(operador) {
+    case "+":
+    resultado = num1 + num2
+    break;
+    case "-":
+    resultado = num1 - num2
+    break;
+    case "*":
+    resultado = num1 * num2
+    break;
+    case "/":
+    resultado = num1 / num2
+    break;
+    case "e":
+      let contador = 1
+      resultado = num1 * num1
+      while (contador < num2 - 1) {
+        resultado = resultado * num1
+        contador++
+      }
+      break;
+  }
 
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  if (resultado == undefined || resultado > 1000000)
+    resultado = "ERRO"
+
+  return resultado
+}
+
 
 let visor = document.getElementById("visor");
 
@@ -26,5 +53,5 @@ function calcular() {
       operador = x
   });
 
-  visor.value = Calculadora(numeros[0], numeros[1], operador)
+  visor.value = calculadora(numeros[0], numeros[1], operador)
 }
